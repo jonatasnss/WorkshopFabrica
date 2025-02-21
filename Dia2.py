09e2427b37a2753a7b46f113948a0800cd394534
jonatasnss <bjnns06@gmail.com>
2025-02-21 15:33:16 -0300

    Desafio Dia2

---
+++ Dia2.py
@@ -0,0 +1,38 @@
+# Variaveis e seus Tipos primitivos
+
+nome = "Jonatas" 
+idade = 25 
+altura = 1,64 
+tem_carteira = True
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Altura: {altura}")
+print(f"tem_carteira: {tem_carteira}")
+
+#Operadores Matemáticos
+    #+ - * / % ** //
+
+#Estruturas Condicionais
+    #if - elif - else
+
+#Estruturas de Repetição
+    #for - while
+
+#Lista, Tuplas, Dicionários e Conjuntos (Set)
+
+numeros = [10, 20, 30, 40]
+print(numeros[1])
+
+#Dicionários
+pessoa = {
+    "nome": "Jonatas",
+    "idade": 25,
+    "cidade": "João Pessoa"
+}
+
+print(pessoa["nome"])
+print(pessoa["idade"])
+print(pessoa["cidade"])
+
+#Conjuntos
